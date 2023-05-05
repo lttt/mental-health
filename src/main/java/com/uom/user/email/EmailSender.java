9aa88ab509828f5ea5eb140e75d37fe5da9d4187
lttt <934955104@qq.com>
2023-05-05 18:39:29 +0800

    init project

---
+++ src/main/java/com/uom/user/email/EmailSender.java
@@ -0,0 +1,5 @@
+package com.uom.user.email;
+
+public interface EmailSender {
+    void send(String to, String email);
+}
---
+++ src/main/java/com/uom/user/student/UserRole.java
@@ -0,0 +1,6 @@
+package com.uom.user.student;
+
+public enum UserRole {
+    USER,
+    ADMIN
+}
